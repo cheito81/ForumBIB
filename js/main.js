d2f009e76a3490023afc0eb5eab6735fedfda82f
Isabel Gómez Garcés <1506074@uab-casaconvalescencia.org>
2018-06-26 14:01:39 +0200

    fotos probando descripcion

--- js/main.js
+++ js/main.js
@@ -3,21 +3,37 @@ $(document).ready(function(){
     linkInterno = $('a[href^="#"]');
     linkInterno.on('click',function(e) {
     e.preventDefault();
+    
     var href = $(this).attr('href');
     $('html, body').animate({ scrollTop : $( href ).offset().top-40 }, 'ease-in');
     });
-     $(window).scroll(function () { 
-        var scroll = $(window).scrollTop();
-        newFunction(scroll);
-    });
-    
+   
+});
+$('a').click(function (e) { 
+        e.preventDefault();
+        $(this).addClass('active');
+});
+/*
+ * Change Navbar color while scrolling
+*/
+
+$(window).scroll(function(){
+	handleTopNavAnimation();
 });
 
-function newFunction(scroll) {
-    if (scroll > 50) {
-        $("nav").addClass(".navbar-solid");
-    }
-    else {
-        $("nav").removeClass(".navbar-solid");
-    }
+$(window).load(function(){
+	handleTopNavAnimation();
+});
+
+function handleTopNavAnimation() {
+	var top=$(window).scrollTop();
+
+	if(top>400){
+        $('#site-nav').addClass('bg-light').fadeIn( "slow" ); 
+        $('#site-nav').addClass('navbar-light').fadeIn( "slow" );
+	}
+	else{
+        $('#site-nav').removeClass('bg-light'); 
+        $('#site-nav').removeClass('navbar-light'); 
+	}
 }
